725153c84b1c3bc5c1027faf61aac57e080ddc76
AwesomeProgrammerStudios <80014866+AwesomeProgrammerStudios@users.noreply.github.com>
2021-03-29 17:22:52 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
-function preload(){
+var Blockhead, BlockheadImg;
 
+function preload(){
+    BlockheadImg = loadImage("Blockhead.png");
 }
 
 
 function setup() {
- 
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  
+    resizeCanvas(windowWidth, windowHeight);
+    background(0);
+    drawSprites();
 }
